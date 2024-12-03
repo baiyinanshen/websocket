434e2bfb94530a783bb63fb35da3bd917d0a1868
baiyinanshen <kininialini@gmail.com>
2024-12-03 07:36:45 +0000

    small change

--- mingling.sh
+++ mingling.sh
@@ -1,4 +1,5 @@
 ./echo_server
+./Liso_server
 ./echo_client 127.0.0.1 9999 ./samples/request_get
 ./echo_client 127.0.0.1 9999 ./samples/request_pipeline
 ./echo_client 127.0.0.1 9999 ./samples/request_pipeline1
